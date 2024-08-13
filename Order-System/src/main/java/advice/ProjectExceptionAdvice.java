47d8df26e6bd8cd3dd60e80db05e929e4f4b16ae
尘心_无愿 <2568237200@qq.com>
2024-08-13 17:25:39 +0800

    全局异常处理

---
+++ Order-System/src/main/java/advice/ProjectExceptionAdvice.java
@@ -0,0 +1,17 @@
+package advice;
+
+import com.zqu.pojo.Result;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice  //用于标识当前类为REST风格对应的异常处理器
+public class ProjectExceptionAdvice {
+
+    //统一处理所有的Exception异常
+    @ExceptionHandler(Exception.class)
+    public Result doOtherException(Exception ex){
+        Result result = new Result(null,"系统错误",500);
+        result.setData(ex.getMessage());
+        return result;
+    }
+}
