7e3495a1c1a86aa64ccd80153657ac0c5bdc04da
尘心_无愿 <2568237200@qq.com>
2024-08-13 17:05:10 +0800

    web修改

--- order-web/src/App.vue
+++ order-web/src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <div id="app">
     <nav>
+      <h1>点餐系统</h1>
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/contact">Contact</router-link> |
+      <router-link to="/admin">Admin</router-link>
+      
     </nav>
     <router-view/>
   </div>
@@ -13,7 +17,7 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
 }
 
--- order-web/src/components/OrderMain.vue
+++ order-web/src/components/OrderMain.vue
@@ -1,34 +1,76 @@
 <template>
-  <div id="app">
-    <el-radio-group v-model="tabPosition" style="margin-bottom: 30px;">
-      <el-radio-button label="top">top</el-radio-button>
-      <el-radio-button label="right">right</el-radio-button>
-      <el-radio-button label="bottom">bottom</el-radio-button>
-      <el-radio-button label="left">left</el-radio-button>
-    </el-radio-group>
-
-    <el-tabs :tab-position="tabPosition" style="height: 200px;">
-      <el-tab-pane label="用户管理" tab-click="/Test1.vue">
-      <router-view/>
+  <div class="tabs-wrapper">
+    <el-tabs
+      :tab-position="tabPosition"
+      v-model="activeTab"
+      class="tabs"
+    >
+      <el-tab-pane
+        v-for="tab in tabs"
+        :label="tab.label"
+        :key="tab.index"
+        :name="tab.index"
+      >
+        <div class="tab-content">
+          <component :is="currentComponent" />
+        </div>
       </el-tab-pane>
-      <el-tab-pane label="配置管理">配置管理</el-tab-pane>
-      <el-tab-pane label="角色管理">角色管理</el-tab-pane>
-      <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
     </el-tabs>
   </div>
-
-
 </template>
-<script>
-  export default {
-    data() {
-      return {
-        tabPosition: 'left'
-      };
-    },
-    components:{
 
+
+<script>
+import Test1 from './Test1.vue';
+// 根据需要导入其他组件
+
+export default {
+  data() {
+    return {
+      tabPosition: 'left',
+      activeTab: '/Test1',
+      tabs: [
+        { label: '用户管理', index: '/Test1' },
+        { label: '配置管理', index: '/Config' },
+        { label: '角色管理', index: '/Role' },
+        { label: '定时任务补偿', index: '/Task' }
+      ]
+    };
+  },
+  computed: {
+    currentComponent() {
+      switch (this.activeTab) {
+        case '/Test1':
+          return Test1;
+        // 添加其他组件的情况
+        default:
+          return null;  // 或者一个默认组件
+      }
     }
-  };
+  },
+  components: {
+    Test1,
+    // 其他组件
+  }
+};
 
 </script>
+
+<style scoped>
+.tabs-wrapper {
+  display: flex;
+  height: 100vh; /* 确保高度足够展示内容和分页按钮 */
+}
+
+.tabs {
+  flex: 1;
+  overflow: hidden;
+}
+
+.tab-content {
+  padding: 20px;
+  height: calc(100vh - 60px); /* 根据需要调整，留出分页按钮的空间 */
+  overflow-y: auto;
+}
+
+</style>
