a305f43d1c57371be4cbed95348acf5a11eea356
尘心_无愿 <2568237200@qq.com>
2024-08-13 17:21:28 +0800

    表现层数据封装

--- Order-System/src/main/java/com/zqu/pojo/User.java
+++ Order-System/src/main/java/com/zqu/pojo/User.java
@@ -1,5 +1,20 @@
 package com.zqu.pojo;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    //用户名
     private String username;
+    //密码
+    private String password;
+    //管理权限
+    private Boolean admin;
+
 }
