01af73103e35a4d27a753eb6bd8db3cf34f503b5
xihen <2738412051@qq.com>
2024-08-27 13:47:44 +0800

    front1

--- order-web/src/App.vue
+++ order-web/src/App.vue
@@ -1,50 +1,15 @@
 <template>
   <div id="app">
-    <div id="title">
-      <h1>点餐系统</h1>
-    </div>
-    <nav>
-      <router-link to="/">主页</router-link> |
-      <router-link to="/about">订单详情</router-link> |
-      <router-link to="/admin" v-if="false">后台管理系统 |</router-link><!--管理员-->
-      <router-link to="/User">关于我的</router-link>
-    </nav>
-    <div id="main"><router-view /></div>
-
+    <router-view></router-view>
   </div>
 </template>
 
+<script>
+export default {
+  name: 'App'
+};
+</script>
+
 <style>
-  #title {
-    background: #3498db;
-    padding: 10px;
-    text-align: left;
-    color: #fff;
-  }
-
-  #main {
-    
-  }
-
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    
-    color: #2c3e50;
-  }
-
-  nav {
-    padding: 30px;
-  }
-
-  nav a {
-    font-weight: bold;
-    color: #2c3e50;
-  }
-
-  nav a.router-link-exact-active {
-    color: #42b983;
-  }
-
+/* 全局样式 */
 </style>
--- order-web/src/router/index.js
+++ order-web/src/router/index.js
@@ -2,40 +2,47 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import MainView from '@/views/MainView.vue'
-import Test1 from '@/components/Test1.vue'
+import Front from '../views/Front.vue'
+import test123 from '@/views/test123.vue'
 import Login from '@/views/Login.vue'
 import User from '@/views/User.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'main',
-    component: MainView
-  },
-  {
-    path: '/user',
-    name: 'user',
-    component: User
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: Login
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+const routes = [{
+        path: '/',
+        name: 'Front',
+        component: Front
+    },
+    {
+        path: '/test123',
+        name: 'test123',
+        component: test123
+    },
+    {
+        path: '/user',
+        name: 'user',
+        component: User
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: Login
+    },
+    {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/AboutView.vue')
+    }
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
+
 
 export default router
