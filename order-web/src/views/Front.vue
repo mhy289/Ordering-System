16a7ab4056bc35389ed3f592c99a02ecc03826b2
尘心_无愿 <2568237200@qq.com>
2024-09-02 17:28:18 +0800

    添加购物车前后端联调

--- order-web/src/views/Front.vue
+++ order-web/src/views/Front.vue
@@ -7,7 +7,8 @@
         <div class="user-actions">
           <el-button type="primary" @click="goToLogin" class="login-button" v-if="!isLoggedIn">登录</el-button>
           <el-button type="success" @click="LoginOut" class="login-button" v-if="isPerson">退出登录</el-button>
-          <el-button type="success" @click="goToPerson" class="login-button" v-if="isPerson">{{this.username}}</el-button>
+          <el-button type="success" @click="goToPerson" class="login-button"
+            v-if="isPerson">{{this.username}}</el-button>
           <el-button @click="goToAdmin">后台管理</el-button>
         </div>
       </div>
@@ -18,22 +19,59 @@
       <div class="content">
         <!-- 左侧Tab栏 -->
         <el-aside width="200px" class="categories">
-          <el-tabs tab-position="left">
-            <el-tab-pane label="推荐">
-              <el-menu :default-active="activeMenu">
+          <el-tabs v-model="activeTabName" @tab-click="handleTabClick" tab-position="left">
+            <el-tab-pane label="推荐" name="recommend">
+              <el-menu :default-active="recommendActiveIndex" @select="handleSelect('recommend',$event)">
                 <el-menu-item index="1" class="menu-item-custom">风味小吃</el-menu-item>
                 <el-menu-item index="2" class="menu-item-custom">特色小炒</el-menu-item>
                 <el-menu-item index="3" class="menu-item-custom">饭后甜点</el-menu-item>
+                <el-menu-item index="4" class="menu-item-custom">主食专区</el-menu-item>
+                <el-menu-item index="5" class="menu-item-custom">冰爽饮品</el-menu-item>
               </el-menu>
             </el-tab-pane>
-            <el-tab-pane label="最新">
-              <!-- 其他分类内容 -->
+            <el-tab-pane label="最新" name="new">
+              <el-menu :default-active="newActiveIndex" @select="handleSelect('new',$event)">
+                <el-menu-item index="1" class="menu-item-custom">新品上市</el-menu-item>
+                <el-menu-item index="2" class="menu-item-custom">经典复刻</el-menu-item>
+                <!-- 其他菜单项 -->
+              </el-menu>
             </el-tab-pane>
-            <el-tab-pane label="热门">
-              <!-- 其他分类内容 -->
+            <el-tab-pane label="热门" name="hot">
+              <el-menu :default-active="hotActiveIndex" @select="handleSelect('hot',$event)">
+                <el-menu-item index="1" class="menu-item-custom">热销套餐</el-menu-item>
+                <el-menu-item index="2" class="menu-item-custom">推荐套餐</el-menu-item>
+                <!-- 其他菜单项 -->
+              </el-menu>
             </el-tab-pane>
           </el-tabs>
         </el-aside>
+
+        <!-- 已废弃 -->
+        <!-- <el-aside width="200px" class="categories">
+          <el-tabs tab-position="left">
+            <el-tab-pane label="推荐">
+              <el-menu :default-active="activeMenu" @select="handleSelect">
+                <el-menu-item index="1" class="menu-item-custom">风味小吃</el-menu-item>
+                <el-menu-item index="2" class="menu-item-custom">特色小炒</el-menu-item>
+                <el-menu-item index="3" class="menu-item-custom">饭后甜点</el-menu-item>
+                <el-menu-item index="4" class="menu-item-custom">主食专区</el-menu-item>
+                <el-menu-item index="5" class="menu-item-custom">冰爽饮品</el-menu-item>
+              </el-menu>
+            </el-tab-pane>
+            <el-tab-pane label="最新">
+              <el-menu :default-active="activeMenu" @select="handleSelect">
+                <el-menu-item index="1" class="menu-item-custom">新品上市</el-menu-item>
+                <el-menu-item index="2" class="menu-item-custom">经典复刻</el-menu-item>
+              </el-menu>
+            </el-tab-pane>
+            <el-tab-pane label="热门">
+              <el-menu :default-active="activeMenu" @select="handleSelect">
+                <el-menu-item index="1" class="menu-item-custom">热销套餐</el-menu-item>
+                <el-menu-item index="2" class="menu-item-custom">推荐套餐</el-menu-item>
+              </el-menu>
+            </el-tab-pane>
+          </el-tabs>
+        </el-aside> -->
 
         <!-- 右侧食品展示区域 -->
         <div class="food-area">
@@ -99,7 +137,10 @@
     data() {
       return {
         searchQuery: '',
-        activeMenu: '1',
+        activeTabName: 'recommend', // 控制激活的标签页  
+        recommendActiveIndex: '1', // 推荐分类的默认激活菜单项  
+        newActiveIndex: '1', // 最新分类的默认激活菜单项  
+        hotActiveIndex: '1', // 热门分类的默认激活菜单项  
         foods: [],
         cart: [],
         isCartVisible: false,
@@ -109,7 +150,8 @@
       };
     },
     beforeMount() {
-      this.queryAllfoods();
+      //this.queryAllfoods();
+      this.handleSelect('recommend',1)
 
     },
     mounted() {
@@ -120,6 +162,33 @@
         let res = await this.$http.get('/dishes');
         if (res.code == 200) {
           this.foods = res.data;
+        }
+      },
+      /* async queryfoodBycondition() {
+        let cod = this.activeMenu
+        let
+        let res = await this.$http.get('/dishes')
+      } */
+      async handleSelect(tabName, event) {
+        console.log(tabName, event)
+        let cod 
+        let index = event
+        // 根据 tabName 来更新对应的 activeIndex  
+        if (tabName === 'recommend') {
+          //this.recommendActiveIndex = index;
+          cod =1
+        } else if (tabName === 'new') {
+          //this.newActiveIndex = index;
+          cod = 2
+        } else if (tabName === 'hot') {
+          //this.hotActiveIndex = index;
+          cod = 3
+        }
+        let res = await this.$http.get('/dishes/cod/'+ cod +'/index/'+ index)
+        if (res.code == 200) {
+          this.foods = res.data;
+        } else {
+            this.$message.error("查询失败")
         }
       },
       //检测登录状态
@@ -134,14 +203,14 @@
           let res = await this.$http.get('/user/token');
           if (res.code == 200) {
             this.username = res.data.username;
-          } else{
+          } else {
             this.$message.error("获取用户信息失败")
           }
         } else {
           this.$message.error("请先登录")
         }
       },
-      LoginOut(){
+      LoginOut() {
         //删除token
         localStorage.removeItem('token');
         this.isLoggedIn = false;
@@ -166,8 +235,11 @@
       search() {
         console.log('搜索内容:', this.searchQuery);
       },
-      addToOrder(food) {
-        const itemIndex = this.cart.findIndex(item => item.name === food.name);
+      async addToOrder(food) {
+        console.log(food)
+        let res = await this.$http.post('/cart/add/' + food.id)
+        console.log(res)
+        /* const itemIndex = this.cart.findIndex(item => item.name === food.name);
         if (itemIndex === -1) {
           this.cart.push({
             ...food,
@@ -175,7 +247,8 @@
           });
         } else {
           this.cart[itemIndex].count += 1;
-        }
+        } */
+
       },
       toggleCart() {
         this.isCartVisible = !this.isCartVisible;
